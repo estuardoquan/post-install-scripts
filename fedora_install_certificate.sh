2105c201ef632a422cc2d3678bda1dc28742a5c2
eq <estuardo.quan@gmail.com>
2026-06-03 18:07:39 -0600

    some changes

--- fedora_install_certificate.sh
+++ fedora_install_certificate.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
-CA_SERVER=${CA_SERVER:-ca.local}
-CA_TRUST=${CA_TRUST:-/etc/pki/ca-trust/source/anchors}
-CA_CRT=${CA_CRT:-roots.pem}
+CERT=${CERT:-ca.local/roots.pem}
+OUT=${OUT:-/etc/pki/ca-trust/source/anchors/roots.pem}
 
-curl -k -o ${CA_TRUST}/${CA_CRT} https://${CA_SERVER}/${CA_CRT}
+INSECURE=${INSECURE:-0}
+UPDATE=${UPDATE:-0}
 
-chmod 644 ${CA_TRUST}/${CA_CRT}
+set -- \
+    -s \
+    -o ${OUT}\
+    https://${CERT}
 
-update-ca-trust
+if [ ${INSECURE} = 1 ]; then
+    set -- $@ \
+        -k
+fi
+
+
+curl $@
+
+if [ $? != 0 ]; then
+    printf "%s\n" ERROR
+    exit 1
+fi
+
+if [ ${UPDATE} = 1 ]; then
+    update-ca-trust
+fi
 
--- fedora_install_systemd-resolved.sh
+++ fedora_install_systemd-resolved.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+DOMAINS=${DOMAINS:-}
+DNS=${DNS:-8.8.8.8}
+FALLBACK=${FALLBACK:-1.1.1.1}
+OUT=${OUT:-/etc/systemd/resolved.conf}
 
+set -- \
+    "[Resolve]" \
+    "DNS=${DNS}" \
+    "FallbackDNS=${FALLBACK}"
+
+if [ -n "${DOMAINS}" ]; then
+    set -- $@ \
+        "Domains=${DOMAINS}"
+fi
+
+printf "%s\n" $@ > ${OUT}
